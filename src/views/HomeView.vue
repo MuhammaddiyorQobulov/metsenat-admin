5a06d0654ca4053aaaf0f0005a343f3e257661e1
muhammaddiyor <muhammaddiyor20032808@gmail.com>
2024-05-29 14:03:02 +0500

    added btn component

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,5 @@
+<script setup></script>
 <template>
   <h1>Home</h1>
 </template>
 
-<script setup></script>
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,3 +1,12 @@
+<script setup>
+import { LogoIcon } from "@/assets/icons/logo";
+import { ref } from "vue";
+const login = ref("");
+const parol = ref("");
+const handleSubmit = () => {
+  console.log(parol.value, login.value);
+};
+</script>
 <template>
   <div class="container">
     <div class="logo">
@@ -8,31 +17,21 @@
     <form @submit.prevent="handleSubmit" class="form">
       <h2>Kirish</h2>
       <div class="input">
-        <label for="login">Login</label>
-        <input v-model="login" type="text" id="login" />
+        <label for="login">LOGIN</label>
+        <input type="text" v-model="login" id="login" />
       </div>
       <div class="input">
-        <label for="parol">Parol</label>
-        <input v-model="parol" type="text" id="parol" />
+        <label for="parol">PAROL</label>
+        <input type="password" v-model="parol" id="parol" />
       </div>
       <button>Kirish</button>
     </form>
   </div>
 </template>
 
-<script setup>
-import { LogoIcon } from "@/assets/icons/logo";
-import { ref } from "vue";
-const login = ref("");
-const parol = ref("");
-const handleSubmit = () => {
-  console.log(parol.value, login.value);
-};
-</script>
-
 <style scoped lang="scss">
 .container {
-  border: 1px solid red;
+  height: 100vh;
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -55,14 +54,30 @@ const handleSubmit = () => {
     border: 1px solid #e0e7ff;
     border-radius: 10px;
     padding: 32px;
+    h2 {
+      padding-bottom: 30px;
+    }
     .input {
       display: flex;
       flex-direction: column;
-      gap: 10px;
+      gap: 5px;
+      margin-top: 20px;
       input {
         border: 1px solid #e0e7ff;
-        background-color: #ebeffd;
+        background-color: #ebeffd81;
         border-radius: 6px;
+        width: 295px;
+        height: 22px;
+        font-size: 16px;
+        padding: 10px;
+        color: #2e384d76;
+      }
+      label {
+        font-size: 12px;
+        font-weight: 500;
+        line-height: 14.22px;
+        letter-spacing: 1.125px;
+        text-align: left;
       }
     }
   }
