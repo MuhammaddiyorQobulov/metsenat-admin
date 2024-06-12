4686b5743c122fd2a773586ec0018e74383d0d1c
muhammaddiyor <muhammaddiyor20032808@gmail.com>
2024-06-12 17:10:18 +0500

    custom table component

---
+++ src/components/CustomTable.vue
@@ -0,0 +1,89 @@
+<template>
+  <table>
+    <thead class="thead">
+      <tr class="header">
+        <th class="title bold-5" v-for="tr in props.head" :key="tr.key">
+          {{ tr.title }}
+        </th>
+      </tr>
+    </thead>
+    <tbody class="tbody">
+      <tr class="trow" v-for="tr in props.body" :key="tr.id">
+        <td v-for="th in head" :key="th.key">
+          <router-link v-if="th.link" :to="'/' + th.link + '/' + tr.id">
+            <EyeIcon />
+          </router-link>
+          <div v-if="th.dataIndex == 'created_at'">
+            {{ moment(tr[th.dataIndex]).format("YYYY-MM-DD") }}
+          </div>
+          <div
+            v-else
+            :class="th.dataIndex == 'get_status_display' && tr[th.dataIndex]"
+          >
+            {{ tr[th.dataIndex] }}
+          </div>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script setup>
+import { EyeIcon } from "@/assets/icons/eye";
+import { defineProps } from "vue";
+const moment = require("moment");
+const props = defineProps({
+  head: Array,
+  body: Array,
+});
+</script>
+
+<style lang="scss" scoped>
+@import "@/styles/variables";
+table {
+  width: 100%;
+  border-collapse: collapse;
+  margin-top: 3rem;
+  th {
+    text-align: left;
+    padding-left: 1rem;
+  }
+  .thead {
+    tr th {
+      padding: 0.5rem 1rem;
+    }
+  }
+  .tbody {
+    tr td {
+      padding: 1rem;
+    }
+
+    .trow {
+      background: $white;
+      border-radius: 10px;
+      border: 1px solid $light-blue;
+      padding: 20px;
+      transition: 0.2s;
+      .Yangi {
+        color: $blue;
+      }
+      .Moderatsiyada {
+        color: $tag-orrange;
+      }
+      .Bekor {
+        color: $grey;
+      }
+      .Tasdiqlangan {
+        color: $green-tag;
+      }
+      .options {
+        cursor: pointer;
+      }
+
+      &:hover {
+        background: $light-blue;
+      }
+    }
+  }
+}
+</style>
--- src/views/Home/sponsor/SponsorsList.vue
+++ src/views/Home/sponsor/SponsorsList.vue
@@ -3,9 +3,8 @@ import PaginationComponent from "@/components/PaginationComponent.vue";
 import { useSponsorStore } from "@/store/sponsors";
 import { onMounted } from "vue";
 import { useI18n } from "vue-i18n";
-import { EyeIcon } from "@/assets/icons/eye";
+import CustomTable from "@/components/CustomTable";
 
-const moment = require("moment");
 const sponsorStore = useSponsorStore();
 const { t } = useI18n();
 onMounted(() => {
@@ -15,44 +14,55 @@ onMounted(() => {
 const HandlePaginate = (page, size) => {
   sponsorStore.getSponsors({ page, size });
 };
+
+const TableHead = [
+  {
+    title: "#",
+    dataIndex: "id",
+    key: "1",
+  },
+  {
+    title: "F.I.SH",
+    dataIndex: "full_name",
+    key: "2",
+  },
+  {
+    title: "TEL.RAQAMI",
+    dataIndex: "phone",
+    key: "3",
+  },
+  {
+    title: "HOMIYLIK SUMMASI",
+    dataIndex: "sum",
+    key: "4",
+  },
+  {
+    title: "SARFLANGAN SUMMA",
+    dataIndex: "spent",
+    key: "5",
+  },
+  {
+    title: "SANA",
+    dataIndex: "created_at",
+    key: "6",
+  },
+  {
+    title: "HOLATI",
+    dataIndex: "get_status_display",
+    key: "7",
+  },
+  {
+    title: "AMALLAR",
+    dataIndex: "actions",
+    key: "8",
+    link: "sponsor-detail",
+  },
+];
 </script>
 
 <template>
   <div class="container" v-if="sponsorStore.datas">
-    <table>
-      <thead class="thead">
-        <tr class="header">
-          <th class="title bold-5">#</th>
-          <th class="title bold-5">{{ t("table.fio") }}</th>
-          <th class="title bold-5">{{ t("table.tel") }}</th>
-          <th class="title bold-5">{{ t("table.sponsor_cost") }}</th>
-          <th class="title bold-5">{{ t("table.spent_cost") }}</th>
-          <th class="title bold-5">{{ t("table.date") }}</th>
-          <th class="title bold-5">{{ t("table.status") }}</th>
-          <th class="title bold-5">{{ t("table.options") }}</th>
-        </tr>
-      </thead>
-      <tbody class="tbody">
-        <tr class="trow" v-for="data in sponsorStore.datas" :key="data">
-          <td>{{ data.id }}</td>
-          <td>{{ data.full_name }}</td>
-          <td>{{ data.phone }}</td>
-          <td>{{ data.sum }}</td>
-          <td>{{ data.spent }}</td>
-          <td>{{ moment(data.created_at).format("YYYY-MM-DD") }}</td>
-          <td :class="data.get_status_display">
-            {{ data.get_status_display }}
-          </td>
-          <td>
-            <div class="options">
-              <router-link :to="`/sponsor-detail/${data.id}`">
-                <eye-icon />
-              </router-link>
-            </div>
-          </td>
-        </tr>
-      </tbody>
-    </table>
+    <custom-table :body="sponsorStore.datas" :head="TableHead" />
     <div class="pagination">
       <p>
         {{ sponsorStore.count.amount }} {{ t("table.shows.from") }}
@@ -66,53 +76,7 @@ const HandlePaginate = (page, size) => {
 
 <style scoped lang="scss">
 @import "@/styles/variables";
-table {
-  width: 100%;
-  border-collapse: collapse;
-  margin-top: 3rem;
-  th {
-    text-align: left;
-    padding-left: 1rem;
-  }
-  .thead {
-    tr th {
-      padding: 0.5rem 1rem;
-    }
-  }
-  .tbody {
-    tr td {
-      padding: 1rem;
-    }
-    margin-top: 20px;
 
-    .trow {
-      background: $white;
-      border-radius: 10px;
-      border: 1px solid $light-blue;
-      padding: 20px;
-      transition: 0.2s;
-      .Yangi {
-        color: $blue;
-      }
-      .Moderatsiyada {
-        color: $tag-orrange;
-      }
-      .Bekor {
-        color: $grey;
-      }
-      .Tasdiqlangan {
-        color: $green-tag;
-      }
-      .options {
-        cursor: pointer;
-      }
-
-      &:hover {
-        background: $light-blue;
-      }
-    }
-  }
-}
 .pagination {
   display: flex;
   justify-content: space-between;
